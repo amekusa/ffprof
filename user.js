64caad8e1d3747c1e2b62165cc61a66dd1d1a052
amekusa <hello@amekusa.com>
2023-02-07 21:52:01 +0900

    Initial commit

---
+++ user.js
@@ -0,0 +1,29 @@
+// display language
+user_pref("intl.accept_languages", "en-us,en,ja");
+
+// location
+user_pref("geo.enabled", false);
+user_pref("geo.wifi.uri", "");
+user_pref("geo.wifi.xhr.timeout", 1);
+user_pref("geo.wifi.logging.enabled", false);
+user_pref("browser.region.network.url", "");
+user_pref("browser.region.update.enabled", false);
+user_pref("browser.search.geoip.url", "");
+user_pref("browser.search.geoip.timeout", 1)
+user_pref("browser.search.geoSpecificDefaults", false);
+user_pref("browser.search.geoSpecificDefaults.url", "");
+user_pref("browser.search.region", "US");
+user_pref("browser.search.isUS", true);
+
+// notification
+user_pref("dom.webnotifications.enabled", false);
+user_pref("dom.webnotifications.serviceworker.enabled", false);
+
+// privacy
+user_pref("beacon.enabled", false);
+user_pref("layout.css.visited_links_enabled", false);
+
+// security
+user_pref("security.mixed_content.block_active_content", true);
+user_pref("dom.serviceWorkers.enabled", false);
+user_pref("network.IDN_show_punycode", true);
